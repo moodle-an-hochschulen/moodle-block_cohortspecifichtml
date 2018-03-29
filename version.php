d65189568d3ca92335a6e06cb0a4be84ec3c415c
Kathrin Osswald <kathrin.osswald@uni-ulm.de>
2018-03-29 13:06:34 +0200

    Release v3.2-r4

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_cohortspecifichtml';
-$plugin->version = 2018020501;
-$plugin->release = 'v3.2-r3';
+$plugin->version = 2018032900;
+$plugin->release = 'v3.2-r4';
 $plugin->requires = 2016120500;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array('block_html' => 2016120500);
