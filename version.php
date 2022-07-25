15a6d4503bf8d839c0420740b986a3a155d746c0
Alexander Bias <bias@alexanderbias.de>
2022-07-25 20:54:44 +0200

    Release v3.9-r6

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_cohortspecifichtml';
-$plugin->version = 2021010603;
-$plugin->release = 'v3.9-r5';
+$plugin->version = 2021010604;
+$plugin->release = 'v3.9-r6';
 $plugin->requires = 2020061510.08;
 $plugin->supported = [39, 39];
 $plugin->maturity = MATURITY_STABLE;
