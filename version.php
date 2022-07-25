f4cc8ca2370f3605d0c4d6715642256397a98b78
Alexander Bias <bias@alexanderbias.de>
2022-07-25 20:54:10 +0200

    Release v3.11-r4

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_cohortspecifichtml';
-$plugin->version = 2021122501;
-$plugin->release = 'v3.11-r3';
+$plugin->version = 2021122502;
+$plugin->release = 'v3.11-r4';
 $plugin->requires = 2021051703.13;
 $plugin->supported = [311, 311];
 $plugin->maturity = MATURITY_STABLE;
