d6eb54bc2351670cfea87c00bd7bf22f68383455
Alexander Bias <bias@alexanderbias.de>
2024-08-18 10:10:32 +0200

    Make codechecker happy again

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -51,7 +51,7 @@ class provider implements \core_privacy\local\metadata\provider,
      * @param collection $collection The initialised collection to add items to.
      * @return collection A listing of user data stored through this system.
      */
-    public static function get_metadata(collection $collection) : collection {
+    public static function get_metadata(collection $collection): collection {
         $collection->link_subsystem('block', 'privacy:metadata:block');
 
         return $collection;
@@ -63,7 +63,7 @@ class provider implements \core_privacy\local\metadata\provider,
      * @param int $userid The user to search.
      * @return contextlist $contextlist The contextlist containing the list of contexts used in this plugin.
      */
-    public static function get_contexts_for_userid(int $userid) : \core_privacy\local\request\contextlist {
+    public static function get_contexts_for_userid(int $userid): \core_privacy\local\request\contextlist {
         // This block doesn't know who information is stored against unless it
         // is at the user context.
         $contextlist = new \core_privacy\local\request\contextlist();
